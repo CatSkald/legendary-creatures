1edd792c51964ed2e5e61c551973ffc344e43b6d
CatSkald <CatSkald@users.noreply.github.com>
2020-05-22 17:23:39 +0200

    Refactor url helpers.

--- src/utils/url-helpers.js
+++ src/utils/url-helpers.js
@@ -1,21 +1,25 @@
+const tagsPath = "/tags";
+const creaturesPath = "/creatures";
+const searchPath = creaturesPath + "/search";
+
+exports.tagsPath = tagsPath;
+exports.creaturesPath = creaturesPath;
+
 exports.localizedSlug = ({ isDefault, locale, slug, isPage }) => {
   if (isPage) {
     return isDefault ? `/${slug}` : `/${locale}/${slug}`;
   }
 
-  return isDefault ? `/creatures/${slug}` : `/${locale}/creatures/${slug}`;
+  return isDefault
+    ? `${creaturesPath}/${slug}`
+    : `/${locale + creaturesPath}/${slug}`;
 };
-
-const tagsPath = "/tags";
-const creaturesPath = "/creatures";
-exports.tagsPath = tagsPath;
-exports.creaturesPath = creaturesPath;
 
 exports.getTagUrl = tag => `${tagsPath}#${tag}`;
 
 exports.getTagValueUrl = (tag, value, pageIndex) => {
-  const url = `${creaturesPath}/search/${tag.toLowerCase()}/${value.toLowerCase()}`;
-  const paginationPath = pageIndex ? `/page/${pageIndex + 1}` : "";
+  const url = `${searchPath}/${tag.toLowerCase()}/${value.toLowerCase()}`;
+  const paginationPath = pageIndex ? `/${pageIndex + 1}` : "";
   return url + paginationPath;
 };
 
@@ -24,6 +28,6 @@ exports.getCreatureUrl = creatureName => {
 };
 
 exports.getCreaturesUrl = pageIndex => {
-  const paginationPath = pageIndex ? `/page/${pageIndex + 1}` : "";
+  const paginationPath = pageIndex ? `/${pageIndex + 1}` : "";
   return creaturesPath + paginationPath;
 };
