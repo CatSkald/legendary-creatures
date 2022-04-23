cefbf8e9cf67054aace639db0b0565b1165511ca
CatSkald <CatSkald@users.noreply.github.com>
2022-04-23 22:53:45 +0200

    Add tests for pagination.

---
+++ src/components/Pagination/index.test.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { LocaleContext } from "../Layout";
+
+import Pagination from "../Pagination";
+import useTranslations from "../../i18n/translations/useTranslations";
+
+import { mockWindowLocation } from "../../../__mocks__/window-location-mock";
+
+mockWindowLocation();
+jest.mock("../../i18n/translations/useTranslations");
+
+useTranslations.mockImplementation(() => ({
+  useTranslations: () => ({
+    Prev: () => "Previous",
+    of: () => "of",
+    Next: () => "Next",
+  }),
+}));
+
+describe("Pagination", () => {
+  test.each([
+    [0, 1, null, null],
+    [0, 2, null, "a"],
+    [1, 2, "b", null],
+    [1, 3, "c", "d"],
+  ])("renders correctly", (currentPage, numPages, prevPage, nextPage) => {
+    const language = { path: "/", code: "en" };
+    const { container } = render(
+      <LocaleContext.Provider value={{ language }}>
+        <Pagination
+          currentPage={currentPage}
+          numPages={numPages}
+          prevPage={prevPage}
+          nextPage={nextPage}
+        />
+        ,
+      </LocaleContext.Provider>,
+    );
+    expect(container).toMatchSnapshot();
+  });
+});
